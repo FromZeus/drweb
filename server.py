34e56621f001b27f91600a284fd7a9808f9d83a4
Daniil Trishkin <daniil.trishkin@qbeon.com>
2017-12-29 00:15:11 +0300

    tmp branch

--- server.py
+++ server.py
@@ -3,5 +3,9 @@
 from app import app
 
 
+from config import HOST, QUEUE, USER, PASSWORD, ROUTING_KEY
+Q = Bus.Queue(HOST, USER, PASSWORD, ROUTING_KEY)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=app.config["APP_PORT"], debug=True)
